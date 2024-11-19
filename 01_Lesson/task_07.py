c0e543093cce8d39e2961051d3d7147edcfc5576
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-19 22:07:17 +0300

    Задача «Парты»
    В школе решили набрать три новых математических класса.
    Так как занятия по математике у них проходят в одно и то же время, было решено выделить
    кабинет для каждого класса и купить в них новые парты. За каждой партой может сидеть
    не больше двух учеников. Известно количество учащихся в каждом из трёх классов.
    Сколько всего нужно закупить парт чтобы их хватило на всех учеников?
    
    Программа получает на вход три натуральных числа: количество учащихся в каждом из трех классов.
    
    Пример:
    тест1 | тест2
      20  |  26
      21  |  20
      22  |  26
    
    Правильный ответ:
      32  |  31

---
+++ 01_Lesson/task_07.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+first_class  = a // 2 + a % 2
+second_class = b // 2 + b % 2
+three_class  = c // 2 + c % 2
+
+total_desk = first_class + second_class + three_class
+
+print(total_desk)
